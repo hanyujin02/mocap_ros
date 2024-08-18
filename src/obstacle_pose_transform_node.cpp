da08f023ad6b737e5fb5e595b10e09b69bda1b13
Zhefan-Xu <zhefanx@andrew.cmu.edu>
2024-08-18 18:31:43 -0400

    modified:   launch/mocap.launch 	new file:   launch/mocap_ob.launch 	modified:   src/obstacle_pose_transform_node.cpp

--- src/obstacle_pose_transform_node.cpp
+++ src/obstacle_pose_transform_node.cpp
@@ -3,6 +3,7 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <thread>
 
 geometry_msgs::PoseStamped initial_pose, initial_pose_world;
 tf2::Transform world_to_map, map_to_world;
@@ -10,7 +11,16 @@ bool initial_pose_received = false;
 
 nav_msgs::Path poseVec;
 // Publisher for the relative map pose
+ros::Publisher obstacle_relative_map_pose_pub;
+std::thread pubWorker;
+int obIdx=0;
+int numOb;
+bool newPoseReceived = false;
+
+
+// Publisher for the relative map pose
 ros::Publisher relative_map_pose_pub;
+ros::Publisher relative_map_obstacle_pose_pub;
 
 // Callback function to receive the pose in the /mavros/vision_pose/pose_raw topic
 void visionPoseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
@@ -50,17 +60,75 @@ void visionPoseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
         relative_pose.pose.orientation.z = rotation.z();
         relative_pose.pose.orientation.w = rotation.w();
 
+
+        relative_pose.header.frame_id = "map";
+        relative_pose.header.stamp = ros::Time::now();
+        relative_map_pose_pub.publish(relative_pose);
+    }
+    catch (tf2::TransformException& ex)
+    {
+        ROS_WARN("Transform exception: %s", ex.what());
+    }
+}
+
+// Callback function to receive the pose in the /mavros/vision_pose/pose_raw topic
+void obstaclePoseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
+{
+    if (!initial_pose_received)
+    {
+        return;
+    }
+
+    try
+    {   
+        geometry_msgs::PoseStamped world_pose = *msg;
+        geometry_msgs::PoseStamped relative_pose;
+
+        // Extract the pose components
+        tf2::Vector3 translation;
+        tf2::Quaternion rotation;
+        tf2::fromMsg(world_pose.pose.position, translation);
+        tf2::fromMsg(world_pose.pose.orientation, rotation);
+
+        // Apply the original transformation
+        translation = map_to_world * translation;
+        rotation = map_to_world * rotation;
+
+        // Convert back to geometry_msgs types
+        relative_pose.pose.position.x = translation.x();
+        relative_pose.pose.position.y = translation.y();
+        relative_pose.pose.position.z = translation.z();
+        relative_pose.pose.orientation.x = rotation.x();
+        relative_pose.pose.orientation.y = rotation.y();
+        relative_pose.pose.orientation.z = rotation.z();
+        relative_pose.pose.orientation.w = rotation.w();
+
         
 
 
         relative_pose.header.frame_id = "map";
         relative_pose.header.stamp = ros::Time::now();
-        poseVec.poses.push_back(relative_pose);
+        poseVec.poses[obIdx] = relative_pose;
+        if (obIdx == numOb-1){
+            obIdx = 0;
+        }
+        else{
+            obIdx++;
+        }
+
         // relative_map_pose_pub.publish(relative_pose);
     }
     catch (tf2::TransformException& ex)
     {
         ROS_WARN("Transform exception: %s", ex.what());
+    }
+}
+void pubCB(void){
+    ros::Rate r(30);
+    while (ros::ok()){
+        poseVec.header.frame_id = "map";
+        obstacle_relative_map_pose_pub.publish(poseVec);
+        r.sleep();
     }
 }
 
@@ -70,19 +138,29 @@ int main(int argc, char** argv)
     ros::NodeHandle nh;
     std::vector<std::string> topics;
     nh.getParam("/mocap/topic_name", topics);
-    
+    numOb = topics.size();
+
+    // Subscribe to the topic that provides the pose in the /mavros/vision_pose/pose_raw topic
+    ros::Subscriber vision_pose_sub = nh.subscribe<geometry_msgs::PoseStamped>(
+        "/mavros/vision_pose/pose_raw", 1, visionPoseCallback);
+
+    // Advertise the new topic for the relative map pose
+    relative_map_pose_pub = nh.advertise<geometry_msgs::PoseStamped>(
+        "mavros/vision_pose/pose", 1);
+
     std::vector<ros::Subscriber> model_state_sub;
-    poseVec.poses.clear();
+    poseVec.poses.resize(numOb);
     for (const auto& topic : topics){
         // Subscribe to the topic that provides the pose in the /mavros/vision_pose/pose_raw topic
         ros::Subscriber sub = nh.subscribe<geometry_msgs::PoseStamped>(
-            topic, 1, visionPoseCallback);
+            topic, 1, obstaclePoseCallback);
         model_state_sub.push_back(sub);
+        
     }
     // Advertise the new topic for the relative map pose
-    relative_map_pose_pub = nh.advertise<nav_msgs::Path>(
-        "/mocap/model_state", 1);
-
+    obstacle_relative_map_pose_pub = nh.advertise<nav_msgs::Path>(
+        "/mocap/model_states", 1);
+    pubWorker = std::thread(&pubCB);
     ros::spin();
 
     return 0;
